ce5720b9b28d4d6d18626311df959ab8279dc290
casgomp <101867902+casgomp@users.noreply.github.com>
2025-12-01 17:37:55 +0100

    joined threads and started modyfing time function.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pecastro <pecastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 11:59:23 by pecastro          #+#    #+#             */
-/*   Updated: 2025/11/17 17:23:45 by pecastro         ###   ########.fr       */
+/*   Updated: 2025/12/01 17:37:38 by pecastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,37 @@
 
 int	main(int argc, char **argv)
 {
-	//unsigned int	time_start;
+	unsigned int	time_start;
 
 	if (argc < 5 || argc > 6)
 		return (print_error_args(1), 1);
+
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	usleep(1000);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+	usleep(1000);
+	time_start = get_time();
+	printf("time_start = %d\n", time_start);
+
 	if (!philo_init(argv))
 		return (1);
 
-	/*time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	usleep(1000);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	usleep(1000);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);*/
 	//cleanup function
 	return (0);
 }
@@ -68,7 +70,14 @@ int	create_threads(t_phargs *phargs)
 		printf("philosopher %d has started\n", i + 1);
 		i ++;
 	}
-	//add pthread_join loop here. Otherwise main continues and likely returns before threads finish doing their thing.
+	i = 0;
+	while (i < phargs->n_philos)
+	{
+		if (pthread_join(phargs->philo[i], NULL) != 0)
+			return (0);
+		printf("philosopher %d has finished execution\n", i + 1);
+		i ++;
+	}
 	return (1);
 }
 
@@ -79,20 +88,23 @@ int	philo_init(char **argv)
 	phargs.n_philos = atoi(argv[1]); //libft and atoi not allowed, so add atoi_protect inside philo project.
 	if (!phargs.n_philos)
 		return (0);
-	if (!create_threads(&phargs))
-		return (0);
 	phargs.time_die = atoi(argv[2]);
 	if (!phargs.time_die)
 		return (0);
+	phargs.time_eat = atoi(argv[3]);	
 	if (!phargs.time_eat)
 		return (0);
+	phargs.time_sleep = atoi(argv[4]);
 	if (!phargs.time_sleep)
 		return (0);
 	if (argv[5])
 	{
+		phargs.n_times_eat = atoi(argv[5]);
 		if (!phargs.n_times_eat)
 			return (0);
 	}
+	if (!create_threads(&phargs))
+		return (0);
 	return (1);
 }
 
