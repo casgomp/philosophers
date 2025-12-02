afa0ce4d9d5795db1839535691763c6673c587ac
casgomp <101867902+casgomp@users.noreply.github.com>
2025-12-02 18:10:20 +0100

    added ft_atoi_safe

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pecastro <pecastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 11:59:23 by pecastro          #+#    #+#             */
-/*   Updated: 2025/12/01 17:37:38 by pecastro         ###   ########.fr       */
+/*   Updated: 2025/12/02 18:10:02 by pecastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,29 +18,6 @@ int	main(int argc, char **argv)
 
 	if (argc < 5 || argc > 6)
 		return (print_error_args(1), 1);
-
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	usleep(1000);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
-	usleep(1000);
-	time_start = get_time();
-	printf("time_start = %d\n", time_start);
 
 	if (!philo_init(argv))
 		return (1);
@@ -85,21 +62,21 @@ int	philo_init(char **argv)
 {
 	t_phargs	phargs;
 
-	phargs.n_philos = atoi(argv[1]); //libft and atoi not allowed, so add atoi_protect inside philo project.
+	phargs.n_philos = ft_atoi_safe(argv[1]); //libft and atoi not allowed, so add atoi_protect inside philo project.
 	if (!phargs.n_philos)
 		return (0);
-	phargs.time_die = atoi(argv[2]);
-	if (!phargs.time_die)
+	phargs.time_die = ft_atoi_safe(argv[2]);//i think i need to malloc first.
+	if (!phargs.time_die) //not sure this is the right way to handle it because atoi can return 0. (only in n_philos its okay to handle because there cant be 0 philosophers)
 		return (0);
-	phargs.time_eat = atoi(argv[3]);	
+	phargs.time_eat = ft_atoi_safe(argv[3]);
 	if (!phargs.time_eat)
 		return (0);
-	phargs.time_sleep = atoi(argv[4]);
+	phargs.time_sleep = ft_atoi_safe(argv[4]);
 	if (!phargs.time_sleep)
 		return (0);
 	if (argv[5])
 	{
-		phargs.n_times_eat = atoi(argv[5]);
+		phargs.n_times_eat = ft_atoi_safe(argv[5]);
 		if (!phargs.n_times_eat)
 			return (0);
 	}
