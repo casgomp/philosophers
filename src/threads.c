dc35ea67d637c0ed3077e8b9ab72fe095da60fc6
pecastro <pecastro@student.42berlin.de>
2025-12-06 13:45:03 +0100

    started with mutex1

--- src/threads.c
+++ src/threads.c
@@ -6,7 +6,7 @@
 /*   By: pecastro <pecastro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/05 14:53:22 by pecastro          #+#    #+#             */
-/*   Updated: 2025/12/05 20:12:14 by pecastro         ###   ########.fr       */
+/*   Updated: 2025/12/06 13:44:48 by pecastro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int	create_threads(t_phargs *phargs)
 	unsigned int	i;
     void            *retval;
 
+	pthread_mutex_init(&phargs->mutex1, NULL);
     if (pthread_create((&phargs->monitor), NULL, &routine_monitor, phargs) != 0)
 		return (perror("Failed to create thread"), 0);
     printf("monitor has started\n");
@@ -26,6 +27,7 @@ int	create_threads(t_phargs *phargs)
 	i = 0;
 	while (i < phargs->n_philos)
 	{
+		phargs->idx_philo = i;
 		if (pthread_create((phargs->philo + i), NULL, &routine_philo, phargs) != 0)
 			return (perror("Failed to create thread"), 0);
 		printf("philosopher %d has started\n", i + 1);
@@ -42,5 +44,6 @@ int	create_threads(t_phargs *phargs)
 		printf("philosopher %d has finished execution\n", i + 1);
 		i ++;
 	}
+	pthread_mutex_destroy(&phargs->mutex1);
 	return (1);
 }
